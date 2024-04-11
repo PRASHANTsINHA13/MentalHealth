419ebc13abdcff0fa4c80b1f8ddfd0c8010130be
reevan leonal dsouza <ree1dsouza@gmail.com>
2024-04-11 21:49:08 +0530

    refactor: refactored db schema

--- frontend/drizzle/0002_rainy_energizer.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE `account`;--> statement-breakpoint
-DROP TABLE `session`;--> statement-breakpoint
-DROP TABLE `verificationToken`;--> statement-breakpoint
-ALTER TABLE user ADD `hashedPassword` text NOT NULL;--> statement-breakpoint
-ALTER TABLE `user` DROP COLUMN `emailVerified`;--> statement-breakpoint
-ALTER TABLE `user` DROP COLUMN `image`;
