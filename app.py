39d91ff66fcf51ab8a75a680cdf532d138f9f6e2
reevan leonal dsouza <ree1dsouza@gmail.com>
2024-04-14 01:07:26 +0530

    feat: refactor ai model code

--- app.py
+++ app.py
@@ -1,158 +1,94 @@
-
 import nltk
-nltk.download('popular')
 from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
 from keras.models import load_model
-model = load_model(r"C:\Users\prash\Mental-health-Chatbot\model.h5")
-
+from flask import Flask, redirect, render_template, request, url_for
+import os
 import json
 import random
 
 
+# When you install the Natural Language Toolkit (NLTK) library, it automatically downloads 
+# and installs a variety of popular NLTK data packages, such as corpora and models.
 
+nltk.download('popular')
 
-from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
-import spacy
-from spacy.language import Language
-from spacy_langdetect import LanguageDetector
-
-
-# translator pipeline for english to swahili translations
-eng_swa_model_checkpoint = "Helsinki-NLP/opus-mt-en-swc"
-eng_swa_tokenizer = AutoTokenizer.from_pretrained("C:\Users\prash\mentalhealthGpt\Mental-health-Chatbot\eng_swa_model")
-eng_swa_model = AutoModelForSeq2SeqLM.from_pretrained("C:\Users\prash\mentalhealthGpt\Mental-health-Chatbot\swa_eng_model")
-
-eng_swa_translator = pipeline(
-    "text2text-generation",
-    model=eng_swa_model,
-    tokenizer=eng_swa_tokenizer,
-)
-
-def translate_text_eng_swa(text):
-    translated_text = eng_swa_translator(text, max_length=128, num_beams=5)[0]['generated_text']
-    return translated_text
-
-# translator pipeline for swahili to english translations
-swa_eng_model_checkpoint = "Helsinki-NLP/opus-mt-swc-en"
-swa_eng_tokenizer = AutoTokenizer.from_pretrained("./model/swa_eng_model/")
-swa_eng_model = AutoModelForSeq2SeqLM.from_pretrained("./model/swa_eng_model/")
-
-swa_eng_translator = pipeline(
-    "text2text-generation",
-    model=swa_eng_model,
-    tokenizer=swa_eng_tokenizer,
-)
-
-def translate_text_swa_eng(text):
-    translated_text = swa_eng_translator(text, max_length=128, num_beams=5)[0]['generated_text']
-    return translated_text
-
-
-def get_lang_detector(nlp, name):
-    return LanguageDetector()
-
-nlp = spacy.load("en_core_web_sm")
-
-Language.factory("language_detector", func=get_lang_detector)
-
-nlp.add_pipe('language_detector', last=True)
+lemmatizer = WordNetLemmatizer()
 
 
 
+# this code picks file model.h5 from current working directory 
+# example path that will be formed C:\Users\user_name\Documents\AI-Mentalchatbot\model.h5
+current_directory = os.getcwd()
+path_to_be_joined = 'model.h5'
+model = load_model(os.path.join(current_directory,path_to_be_joined))
 
 
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('texts.pkl','rb'))
 classes = pickle.load(open('labels.pkl','rb'))
 def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
     sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 def bow(sentence, words, show_details=True):
+    # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
     bag = [0]*len(words)  
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s: 
+                # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
     return(np.array(bag))
 
 def predict_class(sentence, model):
+    # filter out predictions below a threshold
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 def getResponse(ints, intents_json):
-    if ints: 
-        tag = ints[0]['intent']
-        list_of_intents = intents_json['intents']
-        for i in list_of_intents:
-            if i['tag'] == tag:
-                result = random.choice(i['responses'])
-                break
-        return result
-    else:
-        return "Sorry, I didn't understand that."
-
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
 def chatbot_response(msg):
     ints = predict_class(msg, model)
     res = getResponse(ints, intents)
     return res
-from flask import Flask, redirect, render_template, request, url_for
+
+
+# Code for flask server starts from here
+port = 5000
 app = Flask(__name__)
 app.static_folder = 'static'
+
 @app.route("/")
 def home():
     return render_template("index.html")
-@app.route("/login",methods = ['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['email']
-        print(user)
-        return redirect("/")
-    else:
-        user = request.args.get('nm')
-        return render_template("signup.html")
 
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    print("get_bot_response:- " + userText)
+    return chatbot_response(userText)
 
-    doc = nlp(userText)
-    detected_language = doc._.language['language']
-    print(f"Detected language get_bot_response:- {detected_language}")
-
-    bot_response_translate = "Loading bot response..........."  
-
-    if detected_language == "en":
-        bot_response_translate = userText  
-        print("en_sw get_bot_response:-", bot_response_translate)
-        
-    elif detected_language == 'sw':
-        bot_response_translate = translate_text_swa_eng(userText)  
-        print("sw_en get_bot_response:-", bot_response_translate)
-
-    chatbot_response_text = chatbot_response(bot_response_translate)
-
-    if detected_language == 'sw':
-        chatbot_response_text = translate_text_eng_swa(chatbot_response_text)
-
-    return chatbot_response_text
-
-
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == "_main_":
+    app.run(host="0.0.0.0", port=port) # will run server on port 5000
